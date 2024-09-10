7e4dba109a537ab1038f598b95d0765f98ea09d9
Gabe <gabeorlanski@gmail.com>
2024-09-10 11:00:45 -0500

    Swallow IO

--- code_execution/utils.py
+++ code_execution/utils.py
@@ -6,6 +6,7 @@ import errno
 import functools
 import gc
 import inspect
+import io
 import logging
 import multiprocessing as mp
 import os
@@ -187,6 +188,37 @@ def time_limit(seconds: float, on_end: Callable = None):
             yield
         finally:
             signal.setitimer(signal.ITIMER_REAL, 0)
+
+
+# Original code from https://github.com/openai/human-eval/
+class WriteOnlyStringIO(io.StringIO):
+    """StringIO that throws an exception when it's read from"""
+
+    def read(self, *args, **kwargs):
+        raise IOError
+
+    def readline(self, *args, **kwargs):
+        raise IOError
+
+    def readlines(self, *args, **kwargs):
+        raise IOError
+
+    def readable(self, *args, **kwargs):
+        """Returns True if the IO object can be read."""
+        return False
+
+
+class redirect_stdin(contextlib._RedirectStream):  # type: ignore
+    _stream = "stdin"
+
+
+@contextlib.contextmanager
+def swallow_io():
+    stream = WriteOnlyStringIO()
+    with contextlib.redirect_stdout(stream):
+        with contextlib.redirect_stderr(stream):
+            with redirect_stdin(stream):
+                yield
 
 
 SUPPORTED_MODULES = {

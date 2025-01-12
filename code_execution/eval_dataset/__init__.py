5da1a28228e223174157b438f4a80dc899be9c97
gabeorlanski <gabeorlanski@gmail.com>
2025-01-12 16:11:59 -0600

    Init Fix

--- code_execution/eval_dataset/__init__.py
+++ code_execution/eval_dataset/__init__.py
@@ -1,2 +1,4 @@
+from code_execution.eval_dataset import apps
+from code_execution.eval_dataset import gsm8k
 from code_execution.eval_dataset.apps import evaluate as evaluate_apps
 from code_execution.eval_dataset.gsm8k import evaluate as evaluate_gsm8k
